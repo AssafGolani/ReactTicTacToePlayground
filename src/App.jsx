c8ab16e84667c76f0f32b0c5e334f56d32829a3b
Assaf <1214assaf@gmail.com>
2024-02-06 15:15:42 +0200

    Reset the game functionality

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,7 @@ function App() {
     
     const activePlayer = derivedActivePlayer(gameTurns);
 
-    let gameBoard = initialGameBoard;
+    let gameBoard = [...initialGameBoard.map(array => [...array])];
     gameTurns.forEach((turn) => {
       const {square, player} = turn;
       const {row, col} = square;
@@ -65,6 +65,10 @@ function App() {
       });
     }
 
+    function handleRestart(){
+      setGameTurns([]);
+    }
+
     return (
       <>
       <main>
@@ -73,7 +77,7 @@ function App() {
             <Player initialName="Player 1" symbol="X" isActive={activePlayer === 'X'} />
             <Player initialName="Player 2" symbol="O" isActive={activePlayer === 'O'} />
           </ol>
-          { (winner || hasDraw) && <GameOver winner={winner} /> }
+          { (winner || hasDraw) && <GameOver winner={winner} onRestart={handleRestart} /> }
           <GameBoard onSelectSquare={handleSelectSquare} 
           board={gameBoard}  />
         </div>
--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,11 +1,11 @@
 
-export default function GameOver( {winner} ) {
+export default function GameOver( {winner, onRestart} ) {
     return (
         <div id="game-over">
             <h2>Game Over!</h2>
             {winner ? <p>Player {winner} wins!</p> : <p>It's a draw</p>}
             <p>
-                 <button >Reset Game</button>
+                 <button onClick={onRestart}>Reset Game</button>
             </p>
         </div>
     );
